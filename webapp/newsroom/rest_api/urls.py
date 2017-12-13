eb908b18ab66e1bb6f9bb1a57079054027880806
arsho <shovon.sylhet@gmail.com>
2017-12-13 09:23:45 +0600

    repaired merge

--- webapp/newsroom/rest_api/urls.py
+++ webapp/newsroom/rest_api/urls.py
@@ -4,12 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-<<<<<<< Updated upstream
     url(r'^$', views.api_root, name='api_root'),
-=======
-    url(r'^all/$', views.api_root, name='api_all'),
-    url(r'^/$', views.api_index, name='api_index'),
->>>>>>> Stashed changes
     url(r'^news/$', views.NewsList.as_view(), name='news-list'),
     url(r'^news/(?P<pk>[0-9]+)/$', views.NewsDetail.as_view(), name='news-detail'),
     url(r'^news/(?P<pk>[0-9]+)/html/$', views.NewsHtml.as_view(), name='news-detail-html'),
